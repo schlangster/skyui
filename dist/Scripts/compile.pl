9b232841b1133d900b0bb64b4ae7953e06a82371
schlangster <schlangster@gmail.com>
2012-09-08 18:17:41 +0200

    * Added compile script for papyrus source files.

---
+++ dist/Scripts/compile.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+sub error
+{
+	print	"\n=========================\n\n"
+		.	"ERROR: $_[0]\n";
+	getc(STDIN);
+	exit(0);
+}
+
+$skyrimPath			= $ENV{'SkyrimPath'} or error("\$SkyrimPath env var not set");
+$scriptPath			= $skyrimPath . "\\Data\\Scripts\\Source";
+$compilerPath		= $skyrimPath . "\\Papyrus Compiler\\PapyrusCompiler.exe";
+$compilerFlagPath	= $scriptPath . "\\TESV_Papyrus_Flags.flg";
+
+@argList = (
+	"$compilerPath",
+	"Source",
+	"-i=Source; $scriptPath",
+	"-o=.",
+	"-f=$compilerFlagPath",
+	"-op",
+	"-all"
+);
+	
+system(@argList) == 0 or error("Compile failed.");
